De-aliasing Added de-aliasing to the calculation of the rhs function Fixed missing normilization in Fourier interpolation

src/fourier.py
@@ -4,8 +4,9 @@
 #
 # 1) pad    - Pad a 2D array of fourier coefficients
 #             used for interpolation and dealiasing
+# 2) unpad   - Inversion of the pad function
 #
-# 2) interp - Fourier interpolation in 2D
+# 3) interp - Fourier interpolation in 2D
 
 import numpy as np
 import scipy.fft as sfft
@@ -27,6 +28,7 @@ def pad(coefs, shape):
     Ny, Nx = coefs.shape #original
     My, Mx = shape       #new
     assert ( (Nx%2==0) and (Ny%2==0) )
+    assert ( (Mx>=Nx)  and (My>=Ny)  )
 
     #allocate new array
     coefs_pad = np.zeros((My, Mx)).astype('complex128')
@@ -37,7 +39,42 @@ def pad(coefs, shape):
     coefs_pad[:Ny//2,  -Nx//2:] = coefs[:Ny//2,  -Nx//2:]
     coefs_pad[-Ny//2:, -Nx//2:] = coefs[-Ny//2:, -Nx//2:]
 
-    return coefs_pad
+    return (Mx*My/Nx/Ny)*coefs_pad
+
+"""
+Inversion of the pad function (discard higher frequency components)
+
+inputs:
+    coefs_pad   - (2D numpy array) Original Fourier coefficients
+    shape       - (tuple) New number of Fourier modes in x and y directions
+                  Note: the new shape needs to be smaller than the original one.
+
+returns:
+    coefs       - (2D numpy array) Remaining Fourier coefficients
+                  with the higher freqencies discarded.
+"""
+def unpad(coefs_pad, shape):
+    
+    #dimensions
+    Ny, Nx = shape       #new
+    My, Mx = coefs_pad.shape #original
+    assert ( (Nx%2==0) and (Ny%2==0) )
+    assert ( (Mx>=Nx)  and (My>=Ny)  )
+
+    #allocate new array
+    coefs = np.empty((Ny, Nx)).astype('complex128')
+
+    #insert zeros
+    coefs[:Ny//2,  :Nx//2]  = coefs_pad[:Ny//2,  :Nx//2]
+    coefs[-Ny//2:, :Nx//2]  = coefs_pad[-Ny//2:, :Nx//2]
+    coefs[:Ny//2,  -Nx//2:] = coefs_pad[:Ny//2,  -Nx//2:]
+    coefs[-Ny//2:, -Nx//2:] = coefs_pad[-Ny//2:, -Nx//2:]
+
+    #suppress nyquist mode
+    coefs[Ny//2,:] = 0
+    coefs[:,Nx//2] = 0
+
+    return (Nx*Ny/Mx/My)*coefs
 
 """
 Fourier interpolation in 2D. Interpolate a function to a denser grid.

src/vort_rhs.py
@@ -10,6 +10,7 @@ import numpy as np
 import scipy.fft as sfft
 
 from poisson import poisson
+from fourier import interp, unpad
 
 """
 Right hand side function for time stepping of the vorticity equation.
@@ -33,11 +34,6 @@ def vort_rhs(omega, Lx, Ly, Nx, Ny, Uinf):
     l = sfft.fftfreq(Ny)*Ny*2*np.pi/Ly
     kk, ll = np.meshgrid(k, l)
 
-    #Spatial grid
-    x = np.linspace(-Lx/2, Lx/2, Nx, endpoint=False)
-    y = np.linspace(-Ly/2, Ly/2, Ny, endpoint=False)
-    _, yy = np.meshgrid(x, y)
-
     #Reshape omega to 2D array
     omega_2D = np.reshape(omega, (Ny, Nx))
     A = poisson(omega_2D, Lx, Ly, Nx, Ny, Uinf, Fourier=True)
@@ -46,18 +42,21 @@ def vort_rhs(omega, Lx, Ly, Nx, Ny, Uinf):
     u = -1j*sfft.ifft2(A*ll) + Uinf
     v =  1j*sfft.ifft2(A*kk)
 
-    #Compute (omega x u)_x
-    B = sfft.fft2(omega_2D*u)
-    assert ( (Nx%2==0) and (Ny%2==0) )
-    B[int(Ny/2),:] = 0
-    B[:,int(Nx/2)] = 0
+    ### Compute (omega x u)_x
+    #de-aliasing
+    omega_2D_pad = interp(omega_2D, (3*Ny//2, 3*Nx//2))
+    u_pad        = interp(u,        (3*Ny//2, 3*Nx//2))
+    omegau_pad = omega_2D_pad*u_pad
+    B = unpad(sfft.fft2(omegau_pad), (Ny,Nx))
+    #spectral derivative
     omegau_x = 1j*sfft.ifft2(B*kk)
 
-    #Compute (omega x v)_y
-    C = sfft.fft2(omega_2D*v)
-    assert ( (Nx%2==0) and (Ny%2==0) )
-    C[int(Ny/2),:] = 0
-    C[:,int(Nx/2)] = 0
+    ### Compute (omega x v)_y
+    #de-aliasing
+    v_pad = interp(v, (3*Ny//2, 3*Nx//2))
+    omegav_pad = omega_2D_pad*v_pad
+    C = unpad(sfft.fft2(omegav_pad), (Ny,Nx))
+    #spectral derivative
     omegav_y = 1j*sfft.ifft2(C*ll)
 
     #return "flattened" 1D right hand side